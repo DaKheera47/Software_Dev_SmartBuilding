443487e9d6ffdc1920945ad66dd4a5842208addc
DaKheera47 <shaheergoodboy@gmail.com>
2024-03-04 12:07:36 +0000

    L3R4

--- SmartBuildingTests/Level_Three_Tests.cs
+++ SmartBuildingTests/Level_Three_Tests.cs
@@ -7,6 +7,7 @@ namespace SmartBuildingTests
     [TestFixture]
     public class Level_Three_Tests
     {
+        // L3R4
         [TestCase("Lights,OK,OK,OK,", "Doors,OK,OK,OK,", "FireAlarm,OK,OK,OK,", "Lights,OK,OK,OK,Doors,OK,OK,OK,FireAlarm,OK,OK,OK,")]
         [TestCase("Lights,FAULT,OK,OK,", "Doors,OK,FAULT,OK,", "FireAlarm,FAULT,OK,OK,", "Lights,FAULT,OK,OK,Doors,OK,FAULT,OK,FireAlarm,FAULT,OK,OK,")]
         public void GetStatusReport_CombinesManagerStatusesIntoSingleString(string lightStatus, string doorStatus, string fireAlarmStatus, string expectedStatusReport)
@@ -30,32 +31,19 @@ namespace SmartBuildingTests
             Assert.AreEqual(expectedStatusReport, statusReport);
         }
 
-        [Test]
-        public void GetStatus_AllLightsOk_ReturnsAllOkStatus()
+        [TestCase("Lights,OK,OK,OK,")]
+        [TestCase("Lights,FAULT,OK,OK,")]
+        public void GetStatus_AllLightsOk_ReturnsAllOkStatus(string possibleStatus)
         {
             // Arrange
             var lightManager = Substitute.For<ILightManager>();
-            lightManager.GetStatus().Returns("Lights,OK,OK,OK,OK,OK,OK,OK,OK,OK,OK,");
+            lightManager.GetStatus().Returns(possibleStatus);
 
             // Act
             var status = lightManager.GetStatus();
 
             // Assert
-            Assert.AreEqual("Lights,OK,OK,OK,OK,OK,OK,OK,OK,OK,OK,", status);
-        }
-
-        [Test]
-        public void GetStatus_SomeLightsFaulty_ReturnsCorrectStatus()
-        {
-            // Arrange
-            var lightManager = Substitute.For<ILightManager>();
-            lightManager.GetStatus().Returns("Lights,OK,OK,FAULT,OK,OK,OK,OK,FAULT,OK,OK,");
-
-            // Act
-            var status = lightManager.GetStatus();
-
-            // Assert
-            Assert.AreEqual("Lights,OK,OK,FAULT,OK,OK,OK,OK,FAULT,OK,OK,", status);
+            Assert.AreEqual(possibleStatus, status);
         }
     }
 }
